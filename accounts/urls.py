d771c1425d509cef6bc3e8c00af7d78947e6688b
DQM.Codes <46684011+dqmcodes@users.noreply.github.com>
2023-07-11 18:04:31 +0800

    Files Uploaded

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/',views.signuppage,name='signup'),
+    path('login/',views.loginpage,name='login'),
+    path('logout/',views.logoutpage,name='logout'),
+]
